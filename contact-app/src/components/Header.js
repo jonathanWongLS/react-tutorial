a8476fb106388ea98186bbab06e841798c4124c9
JonWLS <jwon0100@student.monash.edu>
2023-04-08 02:09:06 +0800

    Imported Bootstrap to replace Semantic UI

---
+++ contact-app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="position-absolute top-50 start-50 translate-middle">
+        <div className="border border-primary p-3">
+          I am a centered div!
+        </div>
+      </div>
+    );
+};
+
+export default Header;
