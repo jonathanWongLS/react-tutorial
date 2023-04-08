70461502b54d0dfb4edbb0e96c392b40f630bd53
JonWLS <jwon0100@student.monash.edu>
2023-04-09 03:16:54 +0800

    Used useState hook instead of static dictionary of contact names and emails

--- contact-app/src/components/AddContact.js
+++ contact-app/src/components/AddContact.js
@@ -1,22 +1,57 @@
 import React from "react";
 
 class AddContact extends React.Component {
+    state = {
+        name: "",
+        email:"",
+    }
+    
+    add = (e) => {
+        e.preventDefault();
+        if (this.state.name === "" || this.state.email === "") {
+            alert("All the fields are mandatory!");
+            return;
+        }
+
+        console.log(this.state);
+    }
+    
     render() {
         return (
-            <div>
-                <h2 className="mt-3 text-center">Add Contact</h2>
-                <form>
-                    <div className="mb-3">
-                        <label htmlFor="nameInput" className="form-label">Name</label>
-                        <input type="name" placeholder="Enter your name here" className="form-control" id="nameInput" aria-describedby="nameHelp"></input>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="inputEmail" className="form-label">Email</label>
-                        <input type="email" placeholder="Enter your email address here" className="form-control" id="inputEmail"></input>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Add</button>
-                </form>
-            </div>
+          <div>
+            <h2 className="mt-3 text-center">Add Contact</h2>
+            <form onSubmit={this.add}>
+              <div className="mb-3">
+                <label htmlFor="nameInput" className="form-label">
+                  Name
+                </label>
+                <input
+                  type="name"
+                  placeholder="Enter your name here"
+                  className="form-control"
+                  id="nameInput"
+                  value={this.state.name}
+                  onChange={(e) => this.setState({ name: e.target.value })}
+                ></input>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="inputEmail" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your email address here"
+                  className="form-control"
+                  id="inputEmail"
+                  value={this.state.email}
+                  onChange={(e) => this.setState({ email: e.target.value })}
+                ></input>
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Add
+              </button>
+            </form>
+          </div>
         );
     }
 }
--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,23 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 import Header from "./Header"
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-
-  const contacts = [
-    {
-      id: "1",
-      "name": "Ahmed",
-      "email": "ahmed@gmail.com"
-    },
-    {
-      id: "2",
-      "name": "Bobby",
-      "email": "bobby@gmail.com"
-    }
-  ]
+  // useState holds the starting value, in this scenario - an empty list
+  const [contacts, setContacts] = useState([])
 
   return (
     <div className="container">
